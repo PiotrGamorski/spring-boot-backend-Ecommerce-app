923a8f7110aaa80b1f6c8053c8189a97b8d68607
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-06-21 09:48:30 +0200

    endpoint for searching by keyword added.

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -10,7 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    // this is a query method since it starts with findBy - don't need to write your own SQL!
-    // spring will also generate the following endpoint: http://localhost:8080/api/products/search/findByCategoryId?id=...
+    // This is a query method since it starts with findBy - don't need to write your own SQL!
+    // Spring will also generate the following endpoint: http://localhost:8080/api/products/search/findByCategoryId?id=...
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    // SELECT * FROM Product p WHERE P.name LIKE CONCAT('%', :name, '%') -- Spring processes this select behind the scene
+    // Spring will create endpoint:  http://localhost:8080/api/products/search/findByNameContaining?name=...
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
